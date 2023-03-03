8f3c1b926aa9c944fa5c5a9d0b4bbcc8e7098645
william descamps <william.descamps@gmail.com>
2023-03-03 11:48:38 +0100

    Add map

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import streamlit as st
 # TaxiFareModel front
 '''
 
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
+
 st.markdown('''
 Remember that there are several ways to output content into your web page...
 
