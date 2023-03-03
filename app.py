31b171458645adefd9c478c4154bbd49e0fa32d0
william descamps <william.descamps@gmail.com>
2023-03-03 15:28:04 +0100

    upgrade with map, still needs time delays

--- app.py
+++ app.py
@@ -1,54 +1,83 @@
+from geopy.geocoders import Nominatim
 import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+import requests
 
 '''
-# TaxiFareModel front
+# Salut toi ! Besoin d'un tacos?
 '''
+#date and time
+pickup_date = st.date_input(
+    "Which day would you like to be picked up?")
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+pickup_time = st.time_input(
+    "What time would you like to be picked up?")
 
-st.map(df)
+#f'''{pickup_time}'''
+pickup_datetime = str(pickup_date) + str(" ") + str(pickup_time)
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
+#f'''{str(pickup_date)}'''
+#f'''{str(pickup_time)}'''
+#f'''{str(pickup_datetime)}'''
+#pickup_datetime=pd.to_datetime(pickup_datetime)
+#pickup and drop off on map
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
+# @st.cache
+# def get_map_data():
+#     #print('get_map_data called')
+#     return st.map(df)
+#geolocator = Nominatim(user_agent="test")
+#location_start = geolocator.geocode("175 5th Avenue NYC")
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
+geolocator = Nominatim(user_agent="geoapiExercises")
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+#Needs to wait for text input before processing the next part of the code
+#start_=str(st.text_input(label='Start Adress'))
+#geocoded_str = geolocator.geocode(start_)
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+geocoded_str = geolocator.geocode("175 5th Avenue NYC")
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+#f'''{str(geocoded_str.latitude)}, {str(geocoded_str.longitude)}'''
+#print(location.address)
 
-url = 'https://taxifare.lewagon.ai/predict'
+pickup_latitude = str(geocoded_str.latitude)
+pickup_longitude = str(geocoded_str.longitude)
 
-if url == 'https://taxifare.lewagon.ai/predict':
+geocoded_str_destination = geolocator.geocode("Museum of the Moving Image NYC")
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+dropoff_latitude = str(geocoded_str_destination.latitude)
+dropoff_longitude = str(geocoded_str_destination.longitude)
 
-'''
+# passenger count
+passengers = st.slider('How many passengers?',1,8,2)
 
-2. Let's build a dictionary containing the parameters for our API...
+#API call using requests
 
-3. Let's call our API using the `requests` package...
+url = 'https://taxifare.lewagon.ai/predict?'
 
-4. Let's retrieve the prediction from the **JSON** returned by the API...
+#Let's build a dictionary containing the parameters for our API...
+params = {
+    'pickup_latitude': pickup_latitude,
+    'pickup_longitude': pickup_longitude,
+    'dropoff_latitude': dropoff_latitude,
+    'dropoff_longitude': dropoff_longitude,
+    'passenger_count': int(passengers),
+    'pickup_datetime': pickup_datetime
+    }
 
-## Finally, we can display the prediction to the user
-'''
+# Let's call our API using the `requests` package...
+
+if st.button("Let's go !"):
+    response = requests.get(url, params = params)
+    # Let's retrieve the prediction from the **JSON** returned by the API...
+
+    # ## Finally, we can display the prediction to the user
+    df = pd.DataFrame(
+        [(float(pickup_latitude),   float(pickup_longitude)), (float(dropoff_latitude),  float(dropoff_longitude))], columns=['latitude', 'longitude'])
+    response_json=response.json()
+    response_json_fare=response_json.get("fare")
+    f"Your fare will cost around {round(response_json_fare,2)}"
+    st.map(df)
